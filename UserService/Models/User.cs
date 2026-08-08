048e09643f0daca257d5e624b92c1d1af5128719
andrijanastojkovic <andrijanastojkovic10@gmail.com>
2026-08-08 13:01:59 +0200

    Add project files.

---
+++ UserService/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Models
+{
+    public enum UserRole
+    {
+        User,
+        Admin
+    }
+
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public UserRole Role { get; set; } = UserRole.User;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
