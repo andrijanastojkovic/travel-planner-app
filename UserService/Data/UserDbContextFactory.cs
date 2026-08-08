95ee0433eab85dad21c6db1908d95025c3aac677
andrijanastojkovic <andrijanastojkovic10@gmail.com>
2026-08-08 19:02:37 +0200

    Add EF Core, User model, DbContext factory and initial migration for UserService

---
+++ UserService/Data/UserDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace UserService.Data
+{
+    public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+            var connectionString = configuration.GetConnectionString("UserDb");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new UserDbContext(optionsBuilder.Options);
+        }
+    }
+}
